22598b9e5522def2531b81104b72fc4a6ffbea9a
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-10 14:11:34 -0500

    add dbcontext add employee model add nuget entityframework and Identity framework add config: connection string, service migrate to db

---
+++ APIIdentityJWT/Contextes/AuthDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APIIdentityJWT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIIdentityJWT.Contextes
+{
+    public class AuthDemoDbContext : IdentityDbContext
+    {
+        public AuthDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
